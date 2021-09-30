a2dd6092c9f504467bc5e1ded65ab3e0baa8e86e
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-09-30 12:15:35 -0500

    [FIX]: remove 'stories' folder on tsconfig.json

--- build/stories/Dropzone.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-declare const _default: {
-    title: string;
-    component: React.FC<import("../src/components/dropzone/components/Dropzone/DropzoneProps").DropzoneProps>;
-};
-export default _default;
-export declare const Primary: any;
