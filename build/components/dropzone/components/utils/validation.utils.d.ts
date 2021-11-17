216ab117e45b25fd4c16fdfbb5d41b6def5b3bd3
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-11-16 22:50:48 -0500

    [BUILD]: make build for next version

--- build/components/dropzone/components/utils/validation.utils.d.ts
+++ build/components/dropzone/components/utils/validation.utils.d.ts
@@ -23,10 +23,11 @@ export interface FileValidator {
  */
 export declare const separateAccept: (accept: string | undefined) => string[];
 /**
- *
+ * Checks whether a file is valid or not given an array of file extentions and mime types
+ * e.g. accept =  [".doc", ".docx", ".xml", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
  * @param accept the array of strings accept items
  * @param file a File object to be evaluated
- * @returns
+ * @returns true if the mime type file is included in the accept param
  */
 export declare const validateAccept: (accept: string[], file: File) => boolean;
 /**
