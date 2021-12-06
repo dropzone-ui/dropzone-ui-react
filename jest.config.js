4b7a1bfd2162516b8fad74130181f18b10b013af
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-12-06 11:42:31 -0500

    [DEPENDENCIES]: bump dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   roots: ["<rootDir>/tests"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
@@ -8,5 +8,7 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "d.ts"],
   "moduleNameMapper": {
     "^.+\\.(css|less|scss)$": "identity-obj-proxy"
-  }
+  },
+  testEnvironment: "jsdom"
 };
+module.exports =config;
