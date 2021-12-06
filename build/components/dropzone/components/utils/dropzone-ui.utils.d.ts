250172903f1c9dc4c0ac97bec3f81179d7386e79
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-12-06 13:05:08 -0500

    [BUILD]: make build for version 6.3.1

--- build/components/dropzone/components/utils/dropzone-ui.utils.d.ts
+++ build/components/dropzone/components/utils/dropzone-ui.utils.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="react" />
 export declare function createRipple<T extends HTMLButtonElement | HTMLAnchorElement | HTMLDivElement>(event: React.MouseEvent<T, MouseEvent>, color: string): void;
-export declare function createRippleFromElement(element: HTMLDivElement | null, color: string): void;
+export declare function createRippleFromElement<T extends HTMLButtonElement | HTMLAnchorElement | HTMLDivElement>(element: HTMLDivElement | null, event: React.MouseEvent<T, MouseEvent>, color: string): void;
