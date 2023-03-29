f0619ddc20ad19b583d0ef7ed3c003348e8cd906
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2023-03-29 00:22:43 -0500

    [REF]: Change tests

--- tests/Dropone.test.tsx
+++ tests/Dropone.test.tsx
@@ -1,10 +1,10 @@
 import "@testing-library/jest-dom";
 import React from "react";
-import { DropzoneLabel } from "../src";
+import { Dropzone } from "../src";
 
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 
 test("Validate label text must be 'Drop yor files here...'", () => {
-  render(<DropzoneLabel> Drop yor files here...</DropzoneLabel>);
+  render(<Dropzone> Drop yor files here...</Dropzone>);
   expect(screen.getByText("Drop yor files here...")).toBeInTheDocument();
 });
